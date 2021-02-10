c8b55f6c9ec0cee67e1f13695c2156dc93eb1884
daltonteter <54853244+daltonteter@users.noreply.github.com>
2021-02-10 10:45:16 -0600

    Add files via upload

---
+++ SchoolApp/SchoolApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace SchoolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create First Student
+
+            Student FirstStudent = new Student();
+
+            // Get Student Info
+
+            FirstStudent.StudentNum = AskForStudentNumber();
+            FirstStudent.StudentFirstName = AskForStudentName("First");
+            FirstStudent.StudentLastName = AskForStudentName("Last");
+            FirstStudent.Major = AskForMajor(FirstStudent.StudentFirstName);
+            FirstStudent.Score1 = AskForExamScore(1);
+            FirstStudent.Score2 = AskForExamScore(2);
+            FirstStudent.Score3 = AskForExamScore(3);
+
+            Console.Clear();
+            Console.WriteLine("First Student");
+            Console.WriteLine(FirstStudent.ToString());
+
+            Console.WriteLine("Press any key to end");
+            Console.ReadKey();
+
+
+        }
+
+      
+
+        private static int AskForExamScore(int whichOne)
+        {
+            string inValue;
+            int aScore;
+
+            Console.WriteLine("Enter a value for score {0} :", whichOne);
+            inValue = Console.ReadLine();
+            aScore = int.Parse(inValue);
+            return aScore;
+
+        }
+
+        private static string AskForMajor(string studentFirstName)
+        {
+            string inValue;
+            Console.WriteLine("Enter {0}\'s Major: ", studentFirstName);
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+
+        private static string AskForStudentName(string whichOne)
+        {
+            string inValue;
+            Console.WriteLine("Enter Students {0} Name:", whichOne);
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+
+        private static string AskForStudentNumber()
+        {
+            string inValue;
+            Console.WriteLine("Enter Student ID Number: ");
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+    }
+}
---
+++ SchoolApp/SchoolApp/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolApp
+{
+    class Student
+    {
+
+        //Fields /Variables
+
+        private string studentNum, studentLastName, studentFirstName, Major;
+        private int score1, score2, score3;
+
+        //Constructor
+
+        //Default
+
+        public Student()
+        {
+
+        }
+
+        //constructor - One Arg
+        public Student(string sID)
+        {
+            studentNum = sID;
+        }
+
+        //Constructor - Three Arguments
+        
+        public Student(string sID, string lastName, string firstName)
+        {
+            studentNum = sID;
+            studentLastName = lastName;
+            studentFirstName = firstName;
+
+        }
+
+        public Student(string sID, string lastName, string firstName, string Major, int Score1, int Score2, int Score3)
+        {
+            this.studentNum = sID;
+            this.studentLastName = lastName;
+            this.studentFirstName = firstName;
+            this.major = Major;
+            this.score1 = Score1;
+            this.score2 = Score2;
+            this.score3 = Score3;
+        }
+
+        //Properties
+
+        public string StudentNum { get => studentNum; set => studentNum = value; }
+        public string StudentLastName { get => studentLastName; set => studentLastName = value; }
+        private string major { get => major; set => major = value; }
+        public int Score1 { get => score1; set => score1 = value; }
+        public int Score2 { get => score2; set => score2 = value; }
+        public int Score3 { get => score3; set => score3 = value; }
+        public string StudentFirstName { get; internal set; }
+        
+
+
+        //Methods
+
+        public double CalculateAverage()
+        {
+            return (score1 + score2 + score3) / 3.0;
+
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + studentFirstName + " " + studentLastName +
+                " \nMajor: " + Major + "\nScore Average: " + CalculateAverage().ToString("F2");
+        }
+    }
+}
